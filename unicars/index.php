41e84587a09879d43fa995ad7edd9cb17d018a22
istevene <landeste@gmail.com>
2014-06-25 02:23:41 +0200

    aggiunta descrizione

---
+++ unicars/index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Progetto Amministrazione di Sistema</title>
+    </head>
+    <body>
+        <h1>Accesso al progetto</h1>
+        <p>
+            <a href="http://spano.sc.unica.it/amm2014/landeStefano/unicars/php/login">Login</a>
+        </p>
+        <p>
+        <h3>Utenti</h3>
+        <ul>
+            <li>Dipendente
+                <ul>
+                    <li>username: stefano</li>
+                    <li>passowrd: stefano</li>
+                </ul>
+            </li>
+            <li>Cliente
+                <ul>
+                    <li>username: davide</li>
+                    <li>passowrd: davide</li>
+                </ul>
+            </li>
+        </ul>
+    </p>
+
+</body>
+</html>
